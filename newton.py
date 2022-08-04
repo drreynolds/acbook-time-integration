9d489088f56f5a4ff673a8f2af13e0e9ba89cf3f
Daniel R. Reynolds <reynolds@smu.edu>
2022-08-04 09:55:54 -0500

    Refactored Newton solver to take a Jacobian solver constructor (that internally encodes matrix/solver type)

--- newton.py
+++ newton.py
@@ -1,118 +1,38 @@
 # newton.py
 #
-# Module containing functions for performing a modified or inexact Newton nonlinear solver.
-#
 # Daniel R. Reynolds
 # Department of Mathematics
 # Southern Methodist University
 
-def newton_dense(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, lag=0):
+def newton(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, Jfreq=1):
     """
-    Usage: sol = newton_dense(F, J, x0, maxiter=10, rtol=1e-3, atol=1e-6, lag=0)
+    Usage: sol = newton(F, J, x0, maxiter=10, rtol=1e-3, atol=1e-6, Jfreq=1)
 
-    This routine uses a modified Newton method to approximate a root of
-    the nonlinear system of equations F(x)=0.  Here x is an array with n entries, and
-    F(x) is a function that outputs an array with n entries.  The iteration ceases
-    when the following condition is met:
+    This implements a modified Newton method for approximating a root of the nonlinear
+    system of equations F(x)=0.  Here x is a numpy array with n entries, and F(x) is a
+    function that outputs an array with n entries.  The iteration ceases when
 
        || (xnew - xold) / (atol + rtol*|xnew|) ||_RMS < 1
 
     Required inputs:
        * F -- nonlinear residual function, F(x0) should return a numpy array n entries
-       * J -- Jacobian function, J(x0) should return a n*n numpy 2D array
+       * J -- function to construct a scipy.sparse.linalg.LinearOperator object.  This
+              should be callable as  Jsolver = J(x,rtol,abstol), where Jsolver is a
+              scipy.sparse.linalg.LinearOperator object that solves the Newton system
+              dF(x)/dx @ z = r for the numpy array z.  Thus the function J should:
+                1. construct the Jacobian approximation, (dF(x)/dx), for the input x
+                2. set up a linear system solver that takes as input the vector r
+                3. wrap this solver as a scipy.sparse.linalg.LinearOperator object
+              If the solver is iterative, then it should solve the linear system such that
+                   norm(residual) <= max(rtol*norm(r), abstol),
+              where both rtol and abstol are scalar 'float' values.
        * x0 -- initial guess at solution (numpy array with n entries)
 
     Optional inputs:
        * maxiter -- maximum allowed number of iterations (integer >0)
        * rtol -- relative solution tolerance (float, >= 1e-15)
        * atol -- absolute solution tolerance (float or numpy array with n entries, all >=0)
-       * lag -- # iterations to skip before recomputing Jacobian (integer, >=0); note that
-              this is only relevant for dense and sparse linear solvers
-
-    Output: solution dictionary with members
-       * sol['x'] -- the approximate solution (numpy array with n entries)
-       * sol['iters'] -- the number of iterations performed
-       * sol['success'] -- True if iteration converged; False otherwise
-    """
-
-    # imports
-    import numpy as np
-    from scipy.linalg import lu_factor
-    from scipy.linalg import lu_solve
-
-    # initialize output structure
-    sol = {'x': x0, 'iters' : 0, 'success' : False }
-
-    # store nonlinear system size
-    n = x0.size
-
-    # evaluate initial residual
-    F = Ffcn(sol['x'])
-
-    # set up and factor initial Jacobian; return on LU factorization failure
-    J = Jfcn(sol['x'])
-    try:
-        lu, piv = lu_factor(J)
-    except:
-        return sol
-
-    # perform iteration
-    for its in range(1,maxiter+1):
-
-        # increment iteration counter
-        sol['iters'] += 1
-
-        # solve Newton linear system; return on LU solver failure
-        try:
-            h = lu_solve((lu, piv), F)
-        except:
-            return sol
-
-        # compute Newton update, new guess at solution, new residual
-        sol['x'] = sol['x'] - h
-
-        # check for convergence
-        if (np.linalg.norm(h / (atol + rtol*np.abs(sol['x'])))/np.sqrt(n) < 1):
-            sol['success'] = True
-            return sol
-
-        # update nonlinear residual
-        F = Ffcn(sol['x'])
-
-        # update Jacobian every "lag" iterations; return on LU factorization failure
-        if (its % (lag+1) == 0):
-            J = Jfcn(sol['x'])
-            try:
-                lu, piv = lu_factor(J)
-            except:
-                return sol
-
-    # if we made it here, return with current solution (note that sol['success'] is still False)
-    return sol
-
-
-def newton_sparse(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, lag=0):
-    """
-    Usage: sol = newton_sparse(F, J, x0, maxiter=10, rtol=1e-3, atol=1e-6, lag=0)
-
-    This routine uses a modified Newton method to approximate a root of
-    the nonlinear system of equations F(x)=0.  Here x is an array with n entries, and
-    F(x) is a function that outputs an array with n entries.  The iteration ceases
-    when the following condition is met:
-
-      || (xnew - xold) / (atol + rtol*|xnew|) ||_RMS < 1
-
-    Required inputs:
-       * F -- nonlinear residual function, F(x0) should return a numpy array n entries
-       * J -- Jacobian function, J(x0) should return a n*n scipy.sparse matrix
-       * x0 -- initial guess at solution (numpy array with n entries)
-
-    Optional inputs:
-       * maxiter -- maximum allowed number of iterations (integer >0)
-       * rtol -- relative solution tolerance (float, >= 1e-15)
-       * atol -- absolute solution tolerance (float or numpy array with n entries, all >=0)
-       * lag -- # iterations to skip before recomputing Jacobian (integer, >=0); note that
-              this is only relevant for dense and sparse linear solvers
+       * Jfreq -- frequency for calling J (i.e., J is called every Jfreq iterations)
 
     Output: solution structure with members
        * sol['x'] -- the approximate solution (numpy array with n entries)
@@ -122,129 +42,33 @@ def newton_sparse(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, lag=0):
 
     # imports
     import numpy as np
-    from scipy.sparse.linalg import factorized
 
-    # initialize output structure
-    sol = {'x': x0, 'iters' : 0, 'success' : False }
-
-    # store nonlinear system size
-    n = x0.size
-
-    # evaluate initial residual
-    F = Ffcn(sol['x'])
-
-    # set up and factor initial Jacobian; return on LU factorization failure
-    J = Jfcn(sol['x'])
-    try:
-        sparse_solver = factorized(J)
-    except:
-        return sol
-
-    # perform iteration
-    for its in range(1,maxiter+1):
-
-        # increment iteration counter
-        sol['iters'] += 1
-
-        # solve Newton linear system; return on linear solver failure
-        try:
-            h = sparse_solver(F)
-        except:
-            return sol
-
-        # compute Newton update, new guess at solution, new residual
-        sol['x'] = sol['x'] - h
-
-        # check for convergence
-        if (np.linalg.norm(h / (atol + rtol*np.abs(sol['x'])))/np.sqrt(n) < 1):
-            sol['success'] = True
-            return sol
-
-        # update nonlinear residual
-        F = Ffcn(sol['x'])
-
-        # update Jacobian every "lag" iterations
-        if (its % (lag+1) == 0):
-            J = Jfcn(sol['x'])
-            try:
-                sparse_solver = factorized(J)
-            except:
-                return sol
-
-    # if we made it here, return with current solution (note that sol['success'] is still False)
-    return sol
-
-
-
-def newton_gmres(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, Pfcn=0):
-    """
-    Usage: sol = newton_gmres(F, J, x0, maxiter=10, rtol=1e-3, atol=1e-6, P=pcond)
-
-    This routine uses an inexact Newton's method to approximate a root of
-    the nonlinear system of equations F(x)=0.  Here x is an array with n entries, and
-    F(x) is a function that outputs an array with n entries.  The iteration ceases
-    when the following condition is met:
-
-       || (xnew - xold) / (atol + rtol*|xnew|) ||_RMS < 1
-
-    Required inputs:
-       * F -- nonlinear residual function, F(x0) should return a numpy array n entries
-       * J -- Jacobian-vector product function, J(x0,v) should compute the product, J(x0)@v
-       * x0 -- initial guess at solution (numpy array with n entries)
-
-    Optional inputs:
-       * maxiter -- maximum allowed number of iterations (integer >0)
-       * rtol -- relative solution tolerance (float, >= 1e-15)
-       * atol -- absolute solution tolerance (float or numpy array with n entries, all >=0)
-       * P -- if supplied, P(x0,v) should solve the linear system, P(x0)@z = v, where P(x0)
-              is an approximation of the Jacobian
-
-    Output: solution structure with members
-       * sol['x'] -- the approximate solution (numpy array with n entries)
-       * sol['iters'] -- the number of iterations performed
-       * sol['success'] -- True if iteration converged; False otherwise
-    """
-
-    # imports
-    import numpy as np
-    from scipy.sparse.linalg import LinearOperator
-    from scipy.sparse.linalg import gmres
-
-    # initialize output structure
-    sol = {'x': x0, 'iters' : 0, 'success' : False }
-
-    # store nonlinear system size
-    n = x0.size
-
-    # evaluate initial residual
-    F = Ffcn(sol['x'])
-
-    # set up initial Jacobian-vector product, and [optionally] preconditioning operator
-    Jv = lambda v: Jfcn(sol['x'],v)
-    J = LinearOperator((n,n), Jv)
-    if (Pfcn != 0):
-        Prec = lambda v: Pfcn(sol['x'],v)
-        P = LinearOperator((n,n), Prec)
-
-    # set scalar-valued absolute tolerance for GMRES solver
+    # set scalar-valued absolute tolerance for linear solver
     if (np.isscalar(atol)):
         abstol = atol
     else:
         abstol = np.average(atol)
 
+    # initialize output structure
+    sol = {'x': x0, 'iters' : 0, 'success' : False }
+
+    # store nonlinear system size
+    n = x0.size
+
+    # evaluate initial residual
+    F = Ffcn(sol['x'])
+
+    # set up initial Jacobian solver
+    Jsolver = Jfcn(sol['x'], rtol, abstol)
+
     # perform iteration
     for its in range(1,maxiter+1):
 
         # increment iteration counter
         sol['iters'] += 1
 
-        # solve Newton linear system; return with failure on linear solver failure
-        if (Pfcn != 0):
-            h, exitCode = gmres(J, F, tol=rtol, atol=abstol, M=P)
-        else:
-            h, exitCode = gmres(J, F, tol=rtol, atol=abstol)
-        if (exitCode < 0):
-            return sol
+        # solve Newton linear system
+        h = Jsolver.matvec(F)
 
         # compute Newton update, new guess at solution, new residual
         sol['x'] = sol['x'] - h
@@ -257,75 +81,9 @@ def newton_gmres(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, Pfcn=0):
         # update nonlinear residual
         F = Ffcn(sol['x'])
 
+        # update Jacobian every "Jfreq" iterations
+        if (its % Jfreq == 0):
+            Jsolver = Jfcn(sol['x'], rtol, abstol)
+
     # if we made it here, return with current solution (note that sol['success'] is still False)
     return sol
-
-
-
-def newton(Ffcn, Jfcn, x0, maxiter=10, rtol=1e-3, atol=0.0, lag=0, lsolve='dense', Pfcn=0):
-    """
-    Usage: sol = newton(F, J, x0, maxiter=10, rtol=1e-3, atol=1e-6, lag=0, lsolve='dense', P=pcond)
-
-    This is a wrapper routine to linear-algebra-specific implementations of the modified Newton
-    method for dense/sparse matrices, or the inexact Newton method when using the GMRES linear solver.
-
-    We approximate a root of the nonlinear system of equations F(x)=0.  Here x is an array with n
-    entries, and F(x) is a function that outputs an array with n entries.  The iteration ceases
-    when the following condition is met:
-
-       || (xnew - xold) / (atol + rtol*|xnew|) ||_RMS < 1
-
-    Required inputs:
-       * F -- nonlinear residual function, F(x0) should return a numpy array n entries
-       * J -- Jacobian function, with inputs/output dependent on "lsolve":
-              'dense' -- J(x0) should return a n*n numpy 2D array
-              'sparse' -- J(x0) should return a n*n scipy.sparse matrix
-              'iterative' -- J(x0,v) should perform the Jacobian-vector product, J(x0)@v
-       * x0 -- initial guess at solution (numpy array with n entries)
-
-    Optional inputs:
-       * maxiter -- maximum allowed number of iterations (integer >0)
-       * rtol -- relative solution tolerance (float, >= 1e-15)
-       * atol -- absolute solution tolerance (float or numpy array with n entries, all >=0)
-       * lag -- # iterations to skip before recomputing Jacobian (integer, >=0); note that
-              this is only relevant for dense and sparse linear solvers
-       * lsolve -- type of linear algebra to use ('dense', 'sparse', 'iterative')
-       * P -- if supplied, P(x0,v) should solve the linear system, P(x0)@z = v, where P(x0)
-              is an approximation of the Jacobian
-
-    Output: solution structure with members
-       * sol['x'] -- the approximate solution (numpy array with n entries)
-       * sol['iters'] -- the number of iterations performed
-       * sol['success'] -- True if iteration converged; False otherwise
-    """
-
-    # imports
-    import numpy as np
-
-    # overwrite illegal input arguments with default values
-    if (int(maxiter) < 1):
-        maxit = 10
-    if (rtol < 1e-15):
-        rtol = 1e-3
-    if (np.isscalar(atol)):
-        if (atol < 0):
-            atol = 0
-    else:
-        if (np.min(atol) < 0):
-            atol = np.abs(atol)
-    if (lag < 0):
-        lag = 0
-
-    # return an error if x0 isn't a numpy array
-    if (not isinstance(x0, np.ndarray)):
-        raise ValueError('newton error: initial guess x0 must be a numpy array')
-
-    # call the appropriate implementation of Newton's method based on 'lsolve'
-    if (lsolve == 'dense'):
-        return newton_dense(Ffcn, Jfcn, x0, maxiter, rtol, atol, lag)
-    elif (lsolve == 'sparse'):
-        return newton_sparse(Ffcn, Jfcn, x0, maxiter, rtol, atol, lag)
-    elif (lsolve == 'iterative'):
-        return newton_gmres(Ffcn, Jfcn, x0, maxiter, rtol, atol, Pfcn)
-    else:
-        raise ValueError('newton error: lsolve must be one of dense/sparse/iterative')
--- test_newton.py
+++ test_newton.py
@@ -5,14 +5,20 @@ Script to test newton implementations.
 import numpy as np
 import time
 from newton import *
+from scipy.linalg import lu_factor
+from scipy.linalg import lu_solve
 from scipy.sparse import csc_array
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import aslinearoperator
+from scipy.sparse.linalg import gmres
+from scipy.sparse.linalg import factorized
 
 # general parameters for all tests
 maxit = 20
 atols = np.array([1e-8, 1e-10, 1e-12])
 rtols = np.array([1e-2, 1e-4, 1e-6])
 ntols = 3
-lags = [0,1,2]
+lags = [1,2,3]
 
 # set the residual function and various flavors of the Jacobian
 def f(x):              # nonlinear residual
@@ -20,18 +26,38 @@ def f(x):              # nonlinear residual
                   x[1] - 0.004*x[0] + 1e3*x[1]*x[2] + 30.0*x[1]*x[1],
                   x[2] - 30.0*x[1]*x[1]])
     return f
-def Jdense(x):         # dense Jacobian
+def Jac(x):         # dense Jacobian
     J = np.array([[1.004, -1e3*x[2], -1e3*x[1]],
                   [-0.004, 1 + 1e3*x[2] + 60.0*x[1], 1e3*x[1]],
                   [0.0, -60.0*x[1], 1.0]])
     return J
-def Jsparse(x):        # sparse Jacobian
-    return csc_array(Jdense(x))
-def Jtimes(x,v):       # Jacobian-vector product routine
-    J = Jdense(x)
-    return (J @ v)
-def Psolve(x,v):       # preconditioner solver, using diagonal of J
-    return np.array([v[0]/1.004, v[1]/(1 + 1e3*x[2] + 60.0*x[1]), v[2]])
+
+# set up Jacobian solvers
+def Jsolver_dense(x,rtol,abstol):
+    J = Jac(x)
+    try:
+        lu, piv = lu_factor(J)
+    except:
+        raise RuntimeError("Dense Jacobian factorization failure")
+    Jsolve = lambda b: lu_solve((lu, piv), b)
+    return LinearOperator((x.size,x.size), matvec=Jsolve)
+
+def Jsolver_sparse(x,rtol,abstol):
+    J = csc_array(Jac(x))
+    try:
+        Jfactored = factorized(J)
+    except:
+        raise RuntimeError("Sparse Jacobian factorization failure")
+    Jsolve = lambda b: Jfactored(b)
+    return LinearOperator((x.size,x.size), matvec=Jsolve)
+
+def Jsolver_gmres(x,rtol,abstol):
+    Jv = lambda v: Jac(x) @ v
+    J = LinearOperator((x.size,x.size), matvec=Jv)
+    Pinv = np.diag([1.0/1.004, 1.0/(1 + 1e3*x[2] + 60.0*x[1]), 1.0])
+    P = aslinearoperator(Pinv)
+    Jsolve = lambda b: gmres(J, b, tol=rtol, atol=abstol, M=P)[0]
+    return LinearOperator((x.size,x.size), matvec=Jsolve)
 
 # set the intial guess for all tests
 x0 = np.array([0.95, 0.0, 0.01])
@@ -41,15 +67,15 @@ for i in range(ntols):
     print("\nCalling solvers with tolerances: rtol =", rtols[i], ", atol = ", atols[i])
 
     for l in lags:
-        sol = newton(f, Jdense, x0, rtol=rtols[i], atol=atols[i], lag=l, lsolve='dense')
-        print("  dense,  lag=", l,": success =", sol['success'], ", iters =", sol['iters'])
+        sol = newton(f, Jsolver_dense, x0, rtol=rtols[i], atol=atols[i], Jfreq=l)
+        print("  dense,  Jfreq=", l,": success =", sol['success'], ", iters =", sol['iters'])
 
-        sol = newton(f, Jsparse, x0, rtol=rtols[i], atol=atols[i], lag=l, lsolve='sparse')
-        print("  sparse, lag=", l,": success =", sol['success'], ", iters =", sol['iters'])
+        sol = newton(f, Jsolver_sparse, x0, rtol=rtols[i], atol=atols[i], Jfreq=l)
+        print("  sparse, Jfreq=", l,": success =", sol['success'], ", iters =", sol['iters'])
 
-    sol = newton(f, Jtimes, x0, rtol=rtols[i], atol=atols[i], lsolve='iterative', Pfcn=Psolve)
+    sol = newton(f, Jsolver_gmres, x0, rtol=rtols[i], atol=atols[i])
     print("  iterative: success =", sol['success'], ", iters =", sol['iters'])
 
     vatol = atols[i]*np.ones(x0.shape)
-    sol = newton(f, Jdense, x0, rtol=rtols[i], atol=vatol, lag=0, lsolve='dense')
-    print("  dense, lag=0, vector atol: success =", sol['success'], ", iters =", sol['iters'])
+    sol = newton(f, Jsolver_dense, x0, rtol=rtols[i], atol=vatol, Jfreq=1)
+    print("  dense, Jfreq=1, vector atol: success =", sol['success'], ", iters =", sol['iters'])
