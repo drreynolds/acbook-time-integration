d2eaf3a753a9415398098ca78c8176a8324d08fa
Daniel R. Reynolds <reynolds@smu.edu>
2022-08-09 11:11:48 -0500

    Removed obsolete files

--- time_loop.py
+++
@@ -1,9 +0,0 @@
-# time_loop.py
-#
-# Module containing parameters and functions for performing a time evolution loop over a given time interval.
-#
-# Daniel R. Reynolds
-# Department of Mathematics
-# Southern Methodist University
-
-import numpy as np
